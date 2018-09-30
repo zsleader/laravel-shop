3529055013324a3defee4145c40e2584763c5a7c
zsleader <522376982@qq.com>
2018-09-30 06:51:01 +0000

    用户界面-商品列表

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@
 */
 
 Route::get('/', 'PagesController@root')->name('root');
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
@@ -31,6 +33,7 @@ Route::group(['middleware' => 'auth'], function () {
         Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
         Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
         Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+
 
     });
     // 结束
